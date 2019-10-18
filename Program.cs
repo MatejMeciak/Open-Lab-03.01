c0da0fab847f33f71c3af08530c90a52a5dbfd6e
Matej Meciak <matej.meciak123@gmail.com>
2019-10-18 08:57:46 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Open_Lab_03._01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] array = new int[2, 3] { { 1, 1, 1 }, { 1, 1, 1 } };
+            Console.WriteLine(array[1, 2]);
+        }
+    }
+}
